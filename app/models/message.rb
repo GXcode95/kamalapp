98dfb5cc507ea5bde5d3554010dec727643e7a72
GXcode <Xavier.grenouillet@gmail.com>
2024-12-08 01:39:46 +0100

    Refacto: remove actiontext from messages

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,8 +4,6 @@ class Message < ApplicationRecord
   belongs_to :user
   belongs_to :chatroom
 
-  has_rich_text :content
-
   validates :content, presence: true
 
   broadcasts_to ->(message) { message.chatroom }, insert_by: :prepend
